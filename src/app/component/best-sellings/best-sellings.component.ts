d0a7aa05987719a9581acb0f5d1c692e0dbc5fb1
Asandratra <asandratra.rakotondramaka@gmail.com>
2025-06-09 00:11:11 +0300

    First commit

---
+++ src/app/component/best-sellings/best-sellings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-best-sellings',
+  imports: [],
+  templateUrl: './best-sellings.component.html',
+  styleUrl: './best-sellings.component.css'
+})
+export class BestSellingsComponent {
+
+}
---
+++ src/app/component/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { BannerComponent } from "../banner/banner.component";
+import { ArticleListComponent } from "../article-list/article-list.component";
+import { FeaturesComponent } from "../features/features.component";
+import { BestSellingsComponent } from "../best-sellings/best-sellings.component";
+import { NewslettersComponent } from "../newsletters/newsletters.component";
+import { FooterComponent } from "../footer/footer.component";
+import { CopyrightComponent } from "../copyright/copyright.component";
+
+@Component({
+  selector: 'app-home',
+  imports: [HeaderComponent, BannerComponent, ArticleListComponent, FeaturesComponent, BestSellingsComponent, NewslettersComponent, FooterComponent, CopyrightComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
